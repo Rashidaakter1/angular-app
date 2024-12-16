9e57fe071246660dc6ef9b01f04bb119b070679d
Rashidaakter1 <rashidaakterchadni@gamil.com>
2024-12-16 14:48:35 +0600

    working on episodes

--- src/app/pages/episodes/episodes.component.ts
+++ src/app/pages/episodes/episodes.component.ts
@@ -1,19 +1,22 @@
 import { Component } from '@angular/core';
-import { map, Observable } from 'rxjs';
+import { catchError, map, Observable, of } from 'rxjs';
 import { IEpisodes } from '../../core/models/IEpisodes';
 import { RickAndMortyService } from '../../core/services/moduleServices/rick-and-morty-service.service';
 import { HttpParams } from '@angular/common/http';
-import { CommonModule } from '@angular/common';
+import { AsyncPipe, CommonModule } from '@angular/common';
+import { LoaderComponent } from '../../shared/loader/loader.component';
 
 @Component({
   selector: 'app-episodes',
-  imports: [CommonModule],
+  imports: [AsyncPipe, CommonModule, LoaderComponent],
   templateUrl: './episodes.component.html',
   styleUrl: './episodes.component.css',
 })
 export class EpisodesComponent {
   episodes$: Observable<IEpisodes[]> | undefined;
-
+  currentPage: number = 1;
+  totalPages: number = 1;
+  errorMessage: string = '';
   constructor(private rickAndMortyService: RickAndMortyService) {}
   ngOnInit(): void {
     this.getEpisodes();
@@ -21,8 +24,17 @@ export class EpisodesComponent {
 
   getEpisodes() {
     const params = new HttpParams().set('pages', '1');
-    this.episodes$ = this.rickAndMortyService
-      .getEpisodes(params)
-      .pipe(map((result: any) => result.results));
+    this.episodes$ = this.rickAndMortyService.getEpisodes(params).pipe(
+      map((result: any) => {
+        this.totalPages = result.info.pages;
+        return result.results;
+      }),
+      catchError((error) => {
+        this.errorMessage =
+          'Failed to load characters. Please try again later.';
+        console.error('Error occurred:', error);
+        return of([]);
+      })
+    );
   }
 }
