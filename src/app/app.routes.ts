a70ddfcc92f8ac5835f43c0c50a172188d5e7eca
Rashidaakter1 <rashidaakterchadni@gamil.com>
2024-12-16 10:08:26 +0600

    adding services and routes and header file

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,32 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { CharactersComponent } from './pages/characters/characters.component';
+import { EpisodesComponent } from './pages/episodes/episodes.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    title: 'Home',
+    component: CharactersComponent,
+  },
+  {
+    path: 'characters',
+    title: 'Characters',
+    component: CharactersComponent,
+  },
+  {
+    path: 'episodes',
+    title: 'Episodes',
+    component: EpisodesComponent,
+  },
+  {
+    path: 'about',
+    title: 'About',
+    component: EpisodesComponent,
+  },
+  {
+    path: '**',
+    title: '404-Page not found', 
+    component: EpisodesComponent,
+  },
+];
