3fd000b3efdc1aeef305bb5f5247a95038568e5b
Rashidaakter1 <rashidaakterchadni@gamil.com>
2024-12-16 11:16:37 +0600

    working on character api

--- src/app/core/services/moduleServices/rick-and-morty-service.service.ts
+++ src/app/core/services/moduleServices/rick-and-morty-service.service.ts
@@ -1,9 +1,20 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { IResponse } from '../../models/IResponse';
+import { ICharacter } from '../../models/ICharacter';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class RickAndMortyServiceService {
-
-  constructor() { }
+export class RickAndMortyService {
+  constructor(private http: HttpClient) {}
+  getCharacters(params: any): Observable<IResponse> {
+    return this.http.get<IResponse>(
+      'https://rickandmortyapi.com/api/character',
+      {
+        params,
+      }
+    );
+  }
 }
--- src/app/pages/characters/characters.component.ts
+++ src/app/pages/characters/characters.component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RickAndMortyService } from '../../core/services/moduleServices/rick-and-morty-service.service';
+import { ICharacter } from '../../core/models/ICharacter';
+import { map, Observable } from 'rxjs';
+import { HttpParams } from '@angular/common/http';
+import { AsyncPipe, CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-characters',
-  imports: [],
+  imports: [AsyncPipe, CommonModule],
   templateUrl: './characters.component.html',
-  styleUrl: './characters.component.css'
+  styleUrl: './characters.component.css',
 })
-export class CharactersComponent {
+export class CharactersComponent implements OnInit {
+  characters$: Observable<ICharacter[]> | undefined;
 
+  constructor(private rickAndMortyService: RickAndMortyService) {}
+  ngOnInit(): void {
+    this.getCharacter();
+  }
+
+  getCharacter() {
+    const params = new HttpParams().set('pages', '1');
+    this.characters$ = this.rickAndMortyService
+      .getCharacters(params)
+      .pipe(map((result: any) => result.results));
+  }
 }
